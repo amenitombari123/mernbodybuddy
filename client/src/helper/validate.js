b3e255c7eb2851c52780daede577103e475636d8
amenitombari123 <ameni.tombarii@esprit.tn>
2023-10-03 21:31:05 +0100

    password/username

--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -7,6 +7,31 @@ export async function usernameValidate(values){
     return errors;
 }
 
+/** validate password */
+export async function passwordValidate(values){
+    const errors = passwordVerify({}, values);
+
+    return errors;
+}
+
+/** validate password */
+function passwordVerify(errors = {}, values){
+    /* eslint-disable no-useless-escape */
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+    if(!values.password){
+        errors.password = toast.error("Password Required...!");
+    } else if(values.password.includes(" ")){
+        errors.password = toast.error("Wrong Password...!");
+    }else if(values.password.length < 4){
+        errors.password = toast.error("Password must be more than 4 characters long");
+    }else if(!specialChars.test(values.password)){
+        errors.password = toast.error("Password must have special character");
+    }
+
+    return errors;
+}
+
 /**validate username */
 function usernameVerify(error = {}, values){
         if(!values.username){
