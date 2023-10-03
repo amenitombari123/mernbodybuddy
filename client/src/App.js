bd0e0a8ce33f9dec597a06d762ac16d2123ab2f4
amenitombari123 <ameni.tombarii@esprit.tn>
2023-10-03 21:02:37 +0100

    username

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+/**Import all components*/
+import Username from './components/Username';
+import Password from './components/Password';
+import Reset from './components/Reset';
+import Register from './components/Register';
+import Recovery from './components/Recovery';
+import Profile from './components/Profile';
+import PageNotFound from './components/PageNotFound';
 
-function App() {
+/**root routes */
+const router = createBrowserRouter([
+
+    {
+        path : '/',
+        element : <Username></Username>
+    },
+    {
+        path : '/register',
+        element : <Register></Register>
+    },
+    {
+        path : '/Password',
+        element : <Password></Password>
+    },
+    {
+        path : '/profile',
+        element : <Profile></Profile>
+    },
+
+    {
+        path : '/recovery',
+        element : <Recovery></Recovery>
+    },
+
+    {
+        path : '/reset',
+        element : <Reset>register Route</Reset>
+    },
+    {
+        path : '*',
+        element : <PageNotFound>register Route</PageNotFound>
+    },
+])
+
+
+export default function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
-  );
+    <main>
+        
+  <RouterProvider router = {router}></RouterProvider>
+  
+  </main>
+  )
 }
-
-export default App;
