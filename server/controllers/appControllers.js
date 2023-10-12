d671bb163edc70314f1a2163c31bf4cb6e18ba12
amenitombari123 <ameni.tombarii@esprit.tn>
2023-10-12 17:37:54 +0100

    controller

---
+++ server/controllers/appControllers.js
@@ -0,0 +1,37 @@
+/** POST: http://localhost:8080/api/register 
+ * @param : {
+  "username" : "example123",
+  "password" : "admin123",
+  "email": "example@gmail.com",
+  "firstName" : "bill",
+  "lastName": "william",
+  "mobile": 8009860560,
+  "address" : "Apt. 556, Kulas Light, Gwenborough",
+  "profile": ""
+}*/
+
+export async function register(req,res){
+   
+    res.json('register route');
+       
+}
+
+/** POST: http://localhost:8080/api/login 
+ * @param: {
+  "username" : "example123",
+  "password" : "admin123"
+}
+*/
+
+export async function login(req,res){
+   
+    res.json('login route');
+       
+}
+
+
+export async function getUser(req,res){
+   
+    res.json('getUser route');
+       
+}
