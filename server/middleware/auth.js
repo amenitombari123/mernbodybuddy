dc6dceb146018adcac57165e48f45279bf910944
amenitombari123 <ameni.tombarii@esprit.tn>
2023-10-22 19:47:27 +0100

    middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+import ENV from '../config.js';
+
+/** auth middleware */
+export default async function Auth(req, res, next) {
+    try {
+        // Access the Authorization header to validate the request
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader) {
+            return res.status(401).json({ error: "No Authorization header provided." });
+        }
+
+        // Split the Authorization header to extract the token
+        const token = authHeader.split(' ')[1];
+
+        if (!token) {
+            return res.status(401).json({ error: "No token provided." });
+        }
+
+        // Verify the token and decode user details
+        const decodedToken = await jwt.verify(token, ENV.JWT_SECRET);
+
+        req.user = decodedToken;
+
+        // Continue with the next middleware
+        next();
+    } catch (error) {
+        res.status(401).json({ error: "Authentication Failed!" });
+    }
+}
--- server/router/route.js
+++ server/router/route.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 const router = Router();
 /** import all controllers */
 import * as controller from '../controllers/appController.js';
+import Auth from "../middleware/auth.js";
 
 
 /** POST Methods */
@@ -18,7 +19,7 @@ router.route('/createResetSession').get(controller.createResetSession) // reset 
 
 
 /** PUT Methods */
-router.route('/updateuser').put(controller.updateUser); // is use to update the user profile
+router.route('/updateuser').put(Auth, controller.updateUser); // is use to update the user profile
 router.route('/resetPassword').put(controller.resetPassword); // use to reset password
 
 
