a224e88446b18658a2b1689135515cd978e54828
amenitombari123 <ameni.tombarii@esprit.tn>
2023-10-25 14:43:55 +0100

    OTP verification

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import ENV from '../config.js'
 //import mongoose from 'mongoose'; // Import Mongoose
-
+import otpGenerator from 'otp-generator'
 
 /** middleware for verify user */
 export async function verifyUser(req, res, next){
@@ -190,33 +190,64 @@ export async function updateUser(req, res) {
 /** GET: http://localhost:8080/api/generateOTP */
 
 export async function generateOTP(req,res){
-   
-    res.json('generateOTP route');
-       
+  req.app.locals.OTP = await otpGenerator.generate(6, { lowerCaseAlphabets: false, upperCaseAlphabets: false, specialChars: false})
+  res.status(201).send({ code: req.app.locals.OTP })
 }
+
 
 /** GET: http://localhost:8080/api/verifyOTP */
-
 export async function verifyOTP(req,res){
-   
-    res.json('verifyOTP route');
-       
+  const { code } = req.query;
+  if(parseInt(req.app.locals.OTP) === parseInt(code)){
+      req.app.locals.OTP = null; // reset the OTP value
+      req.app.locals.resetSession = true; // start session for reset password
+      return res.status(201).send({ msg: 'Verify Successsfully!'})
+  }
+  return res.status(400).send({ error: "Invalid OTP"});
 }
+
 
 // successfully redirect user when OTP is valid
 /** GET: http://localhost:8080/api/createResetSession */
-
 export async function createResetSession(req,res){
-   
-    res.json('createResetSession route');
-       
+  if(req.app.locals.resetSession){
+   req.app.locals.resetSession = false; //allow acess only this route
+       return res.status(201).send({ msg:"access granted!"})
+  }
+  return res.status(440).send({error : "Session expired!"})
 }
 
 // update the password when we have valid session
 /** PUT: http://localhost:8080/api/resetPassword */
 
-export async function resetPassword(req,res){
-   
-    res.json('resetPassword route');
-       
+
+export async function resetPassword(req, res) {
+  try {
+    if (!req.app.locals.resetSession) {
+      return res.status(440).send({ error: "Session expired!" });
+    }
+
+    const { username, password } = req.body;
+
+    try {
+      const user = await UserModel.findOne({ username });
+
+      if (!user) {
+        return res.status(404).send({ error: "Username not found" });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      await UserModel.updateOne({ username: user.username }, { password: hashedPassword });
+
+      req.app.locals.resetSession = false; // Reset session
+
+      return res.status(201).send({ msg: "Record Updated...!" });
+    } catch (error) {
+      return res.status(500).send({ error: "An error occurred while updating the record" });
+    }
+  } catch (error) {
+    return res.status(401).send({ error });
+  }
 }
+
