717f60c35214f56ef095659001d01ea05e1fa1b7
amenitombari123 <ameni.tombarii@esprit.tn>
2023-10-30 22:12:06 +0100

    update/registeranpassword

--- client/src/components/Password.js
+++ client/src/components/Password.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link , useNavigate} from 'react-router-dom';
 import avatar from '../assets/profile.png';
-import { Toaster } from 'react-hot-toast';
+import toast,{ Toaster } from 'react-hot-toast';
 import { useFormik } from 'formik';
 import { passwordValidate } from '../helper/validate';
 import useFetch from '../hooks/fetch.hook';
 import { useAuthStore } from '../store/store';
+import {verifyPassword}from'../helper/helper'
 import styles from '../styles/Username.module.css';
 
+
 export default function Password() {
+  const navigate = useNavigate()
   const { username } = useAuthStore((state) => state.auth);
   const [{ isLoading, apiData, serverError }] = useFetch(`/user/${username}`);
 
@@ -20,9 +23,19 @@ export default function Password() {
     validateOnBlur: false,
     validateOnChange: false,
     onSubmit: async (values) => {
-      console.log(values);
+      let loginPromise = verifyPassword({ username, password : values.password })
+      toast.promise(loginPromise, {
+        loading: 'Checking...',
+        success : <b>Login Successfully...!</b>,
+        error : <b>Password Not Match!</b>
+      });
+      loginPromise.then(res => {
+        let { token } = res.data;
+        localStorage.setItem('token', token);
+       navigate('/profile')
+      })
     },
-  });
+  })
 
   if (isLoading) return <h1 className='text-2xl font-bold'>isLoading</h1>;
   if (serverError) return <h1 className='text-xl text-red-500'>{serverError.message}</h1>;
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,87 +1,88 @@
-import React, { useState } from 'react';
-import {Link} from 'react-router-dom'
-import avatar from'../assets/profile.png';
-import {Toaster} from 'react-hot-toast';
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import avatar from '../assets/profile.png';
+import toast, { Toaster } from 'react-hot-toast';
 import { useFormik } from 'formik';
-import { passwordValidate } from '../helper/validate';
-import styles from'../styles/Username.module.css';
-import convertToBase64 from '../helper/convert';
 import { registerValidation } from '../helper/validate';
+import convertToBase64 from '../helper/convert';
+import { registerUser } from '../helper/helper'
 
+
+import styles from '../styles/Username.module.css';
 
 export default function Register() {
 
+  const navigate = useNavigate()
   const [file, setFile] = useState()
 
   const formik = useFormik({
     initialValues : {
-      email:'exemple.bb@gmail.com',
-      username:'exemple123',
+      email: 'doyol56239@cnogs.com',
+      username: 'example123',
       password : 'admin@123'
     },
     validate : registerValidation,
     validateOnBlur: false,
     validateOnChange: false,
-    onSubmit : async values =>{ 
-      values = await Object.assign(values,{ profile : file || ''})
-      console.log( values)
+    onSubmit : async values => {
+      values = await Object.assign(values, { profile : file || ''})
+      let registerPromise = registerUser(values)
+      toast.promise(registerPromise, {
+        loading: 'Creating...',
+        success : <b>Register Successfully...!</b>,
+        error : <b>Could not Register.</b>
+      });
+
+      registerPromise.then(function(){ navigate('/')});
     }
   })
 
-    /** formik doensn't support file upload so we need to create this handler */
-    const onUpload = async e => {
-      const base64 = await convertToBase64(e.target.files[0]);
-      setFile(base64);
-    }
+  /** formik doensn't support file upload so we need to create this handler */
+  const onUpload = async e => {
+    const base64 = await convertToBase64(e.target.files[0]);
+    setFile(base64);
+  }
 
   return (
-    <div className= "container mx-auto">
+    <div className="container mx-auto">
 
       <Toaster position='top-center' reverseOrder={false}></Toaster>
 
-    <div className= 'flex justify-center items-center h-screen'>
-       <div className={styles.glass} style={{ width:"45%" , paddingTop: '3em'}} >
+      <div className='flex justify-center items-center h-screen'>
+        <div className={styles.glass} style={{ width: "45%", paddingTop: '3em'}}>
+
           <div className="title flex flex-col items-center">
- 
-          <h4 className='text-5xl font-bold'> Register</h4>
-          <span className='py-4 text-xl w-2/3 text-center text-gray-500'>
-           Glad you've joined Us ! &#9829;
-          </span>
-
-       <form className='py-1' onSubmit={formik.handleSubmit}>
-
-         <div className='profile flex justify-center py-4'>
-
-            <label htmlFor="profile">
-            <img src ={file || avatar} className={styles.profile_img} alt="avatar"/>
-            </label>
-
-            <input onChange={onUpload} type="file" id="profile" name="profile"/>
-
+            <h4 className='text-5xl font-bold'>Register</h4>
+            <span className='py-4 text-xl w-2/3 text-center text-gray-500'>
+                Happy to join you!
+            </span>
           </div>
 
-            <div className="textbox flex flex-col items-center gap-6">
+          <form className='py-1' onSubmit={formik.handleSubmit}>
+              <div className='profile flex justify-center py-4'>
+                  <label htmlFor="profile">
+                    <img src={file || avatar} className={styles.profile_img} alt="avatar" />
+                  </label>
+                  
+                  <input onChange={onUpload} type="file" id='profile' name='profile' />
+              </div>
 
-              <input {...formik.getFieldProps('email')} className={styles.textbox} type="text" placeholder='Email*'/>
-              <input {...formik.getFieldProps('username')} className={styles.textbox} type="text" placeholder='Username*'/>
-              <input {...formik.getFieldProps('password')} className={styles.textbox} type="text" placeholder='password'/>
+              <div className="textbox flex flex-col items-center gap-6">
+                  <input {...formik.getFieldProps('email')} className={styles.textbox} type="text" placeholder='Email*' />
+                  <input {...formik.getFieldProps('username')} className={styles.textbox} type="text" placeholder='Username*' />
+                  <input {...formik.getFieldProps('password')} className={styles.textbox} type="text" placeholder='Password*' />
+                  <button className={styles.btn} type='submit'>Register</button>
+              </div>
 
-              <button className = {styles.btn} type ='submit'> Register </button>
+              <div className="text-center py-4">
+                <span className='text-gray-500'>Already Register? <Link className='text-red-500' to="/">Login Now</Link></span>
+              </div>
 
-            </div>
-            <div className="text-center py-4">
-              <span className='text-gray-500'> Already have an account? <Link className ='text-red-500' to='/'> Login in Now </Link> </span>
+          </form>
 
-            </div>
-            
-          
-           </form>
-          
         </div>
-     </div>
-  </div>
-</div>
+      </div>
+    </div>
   )
 }
-
 
