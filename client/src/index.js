ffd095b2b0a129d4f599e8d811dbdd644c494629
amenitombari123 <ameni.tombarii@esprit.tn>
2023-11-14 16:13:58 +0100

    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,23 @@
-import react from "react";
-import ReactDOM from "react-dom/client";
+// src/index.js
+import React from "react";
+import ReactDOM from "react-dom";
 import './index.css';
-import App from './App'
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <>
+import App from './App';
+import Logo from './assets/images/Logo.png'; // Updated path
+
+document.title = 'BodyBuddy';
+
+const root = document.getElementById('root');
+
+ReactDOM.render(
+    <React.StrictMode>
         <App />
-    </>
-)
+    </React.StrictMode>,
+    root
+);
+
+// Optionally, set the favicon dynamically
+const favicon = document.querySelector("link[rel*='icon']") || document.createElement('link');
+favicon.type = 'image/png';
+favicon.href = Logo; // Use the imported logo path
+document.head.appendChild(favicon);
